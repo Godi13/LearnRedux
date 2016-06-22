760f64e72485fae2afa353f678d433d591e5d071
godi13 <godi13@vip.qq.com>
2016-06-22 18:03:41 +0800

    HelloWorld

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 var webpack = require('webpack');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    a: './src/a.js'
+  },
   output: {
     path: './dist/',
-    filename: 'index.js'
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'stage-0', 'react']
+      }
+    }]
   }
 }
