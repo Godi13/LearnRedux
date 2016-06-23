a1d68c07d0c9c294e2d901b284d14ff0cdabbfb4
godi13 <godi13@vip.qq.com>
2016-06-23 22:08:00 +0800

    removeCounterBeforeDeepFreeze

--- src/index.js
+++ src/index.js
@@ -2,19 +2,36 @@ import except from 'expect';
 import deepFreeze from 'deep-freeze';
 
 const addCounter = (list) => {
-  return list.concat(0);
+  return [...list, 0]
+}
+
+const removeCounter = (list, index) => {
+  list.splice(index, 1);
+  return list;
 }
 
 const testAddCounter = () => {
   const listBefore = [];
   const listAfter = [0];
 
-  deepFreeze(listBefore)
+  deepFreeze(listBefore);
 
   except(
     addCounter(listBefore)
   ).toEqual(listAfter);
 };
 
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);
+
+  except(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+}
+
 testAddCounter();
+testRemoveCounter();
 console.log('All tests pass');
