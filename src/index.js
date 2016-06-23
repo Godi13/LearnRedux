f2d0b6998d625dead6901140b1351385f8f7cf8b
godi13 <godi13@vip.qq.com>
2016-06-23 21:53:22 +0800

    testExpect

--- src/index.js
+++ src/index.js
@@ -1,50 +1,18 @@
-import { createStore } from 'redux';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import except from 'expect';
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+const addCounter = (list) => {
+  list.push(0);
+  return list;
 }
 
-const Counter = ({
-  value,
-  onIncrement,
-  onDecrement
-}) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
 
-const store = createStore(counter);
-
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() =>
-        store.dispatch({
-          type: 'INCREMENT'
-        })
-      }
-      onDecrement={() =>
-        store.dispatch({
-          type: 'DECREMENT'
-        })
-      }
-    />,
-    document.getElementById('root')
-  );
+  except(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
 };
 
-store.subscribe(render);
-render()
+testAddCounter();
+console.log('All tests pass');
