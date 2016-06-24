c656c721e8cdfa3500c9da968815fc48f71af07b
godi13 <godi13@vip.qq.com>
2016-06-24 19:37:20 +0800

    simpleCombineReducers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -48,18 +48,20 @@ const visibilityFilter = (
   }
 };
 
-// const todoApp = (state = {}, action) => {
-//   return {
-//     todos: todos(
-//       state.todos,
-//       action
-//     ),
-//     visibilityFilter: visibilityFilter(
-//       state.visibilityFilter,
-//       action
-//     )
-//   };
-// };
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        );
+        return nextState;
+      },
+      {}
+    );
+  };
+};
 
 const todoApp = combineReducers({
   todos,
