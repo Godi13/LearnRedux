b989ac01bd9cc4629d6fd495a7f81a923a347894
godi13 <mqzq9388@gmail.com>
2017-01-04 10:46:25 +0800

    use lodash to save expensive JSON.stringify

--- todoList/index.js
+++ todoList/index.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
 import { saveState, loadState } from './localStoreage';
+import throttle from 'lodash/throttle';
 
 const persistedState = loadState();
 const store = createStore(
@@ -12,11 +13,11 @@ const store = createStore(
   persistedState
 )
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos
   })
-})
+}, 1000))
 
 render(
   <Provider store={store}>
